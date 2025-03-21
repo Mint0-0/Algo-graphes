375b6a24dc7d9d020bb701af2abd8765de7a1e49
Mint0-0 <amy98764321@gmail.com>
2025-03-21 16:30:48 -0400

    lettre pour les noeuds

--- graphe.py
+++ graphe.py
@@ -1,14 +1,19 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 import random
+import string
 
 # genere un graphe aleatoire
 def generer_graphe(nb_noeuds, vertice_proba, option_poids):
     G = nx.gnp_random_graph(nb_noeuds, vertice_proba)
-    # si on veit des poids aux vertices
-#    if option_poids:
-# 	for() in G.edges():
-#	    G[][]['weigth'] = random.randint(1, 15)
+    # lettre pour les noeuds
+    mapping = {i: string.ascii_uppercase[i] for i in range(nb_noeuds)}
+    G = nx.relabel_nodes(G, mapping)
+    
+    # si on veut des poids aux vertices
+    if option_poids:
+        for u, v  in G.edges():
+            G[u][v]['weight'] = random.randint(1, 15)
     return G
 
 # noeuds -> nx
@@ -18,5 +23,5 @@ def afficher_graphe(G, poids=False):
     plt.show()
 
 if __name__ == "__main__":
-    G = generer_graphe(random.randint(10,15), 0.3, False)
+    G = generer_graphe(random.randint(10,15), 0.3, True)
     afficher_graphe(G)
