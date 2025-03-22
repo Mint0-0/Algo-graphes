977042c21a85bd58e8185ea6a36dd38a854d2079
Mint0-0 <amy98764321@gmail.com>
2025-03-21 20:39:48 -0400

    changement  des couleurs et début de l'ajout de l'option poids

--- graphe.py
+++ graphe.py
@@ -13,15 +13,28 @@ def generer_graphe(nb_noeuds, vertice_proba, option_poids):
     # si on veut des poids aux vertices
     if option_poids:
         for u, v  in G.edges():
-            G[u][v]['weight'] = random.randint(1, 15)
+            G[u][v]['weight'] = random.randint(1, 10)
     return G
 
 # noeuds -> nx
 # lignes -> plt
-def afficher_graphe(G, poids=False):
-    nx.draw(G, with_labels=True, node_color="pink", edge_color="gray")
+def afficher_graphe(G, option_poids):
+    couleurs = ["#CCFF66" if node == "A" else "#FFCC99" for node in G.nodes()]
+
+    pos = nx.spring_layout(G, seed = 42)
+    
+    nx.draw(G, with_labels=True, node_color=couleurs, edge_color="gray")
+    if option_poids:
+        poids_vertice = {}
+        for u, v, data in G.edges(data = True):
+            poids_vertice[(u, v)] = data['weight']
+        nx.draw_networkx_edge_labels(G, pos, edge_labels=poids_vertice)
+    
     plt.show()
 
+
+def parcours_profondeur(G):
+    
 if __name__ == "__main__":
-    G = generer_graphe(random.randint(10,15), 0.3, True)
-    afficher_graphe(G)
+    G = generer_graphe(random.randint(5,10), 0.3, False)
+    afficher_graphe(G, False)
