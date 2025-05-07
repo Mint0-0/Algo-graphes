8054a8256ee5f05cc6c11a346435c18b14b85c01
Mint0-0 <amy98764321@gmail.com>
2025-05-07 19:30:36 -0400

    refactoring, séparation des parcours de graphe du code de génération de graphe

--- graphe.py
+++ graphe.py
@@ -6,7 +6,6 @@ import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 import random
 import string
-import heapq
 
 app = dash.Dash(__name__)
 cyto.load_extra_layouts()
@@ -109,47 +108,6 @@ def afficher_graphe(G, option_poids):
         nx.draw_networkx_edge_labels(G, pos, edge_labels=poids_vertice)
     
     plt.show()
-
-
-
-
-# PARCOURS EN PROFONDEUR
-# le sommet de départ sera toujours 'A' et si le graphe n'est pas connexe on y va en ordre alphabetique
-def parcours_profondeur(G):
-    for noeud in G.nodes:       
-        if G.nodes[noeud]['marque'] == False:
-            visiter_profondeur(G, noeud)
-   
-def visiter_profondeur(G, noeud):
-    G.nodes[noeud]['marque'] = True
-    print(noeud)
-    for voisin in G.neighbors(noeud):
-        if G.nodes[voisin]['marque'] == False:
-            G.nodes[voisin]['parent'] = noeud
-            G.nodes[voisin]['distance'] = G.nodes[noeud]['distance'] + 1
-            visiter_profondeur(G, voisin)
-    
-def parcours_largeur(G): 
-     for noeud in G.nodes: 
-        if G.nodes[noeud]['marque'] == False:
-            visiter_largeur(G, noeud)
-    
-def visiter_largeur(G, noeud):
-    G.nodes[noeud]['marque'] = True
-    print(noeud)
-    file = []
-    heapq.heappush(file, noeud)
-    while file:
-        noeud = heapq.heappop(file)
-        for voisin in G.neighbors(noeud):
-            if G.nodes[voisin]['marque'] == False:
-                G.nodes[voisin]['parent'] = noeud
-                G.nodes[voisin]['distance'] = G.nodes[noeud]['distance'] + 1
-                G.nodes[voisin]['marque'] = True
-                heapq.heappush(file, voisin)
-                print(voisin)
-            
-        
     
 if __name__ == "__main__":
    # G = generer_graphe(random.randint(5,10), 0.3, False)
